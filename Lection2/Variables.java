ed6546280513099b245981c76ba72f99bd13e2b0
J-RCS <jurijs.grabovskis@gmail.com>
2019-10-30 22:54:49 +0200

    Session 2 live-coding results

---
+++ Lection2/Variables.java
@@ -0,0 +1,34 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello %useranme% !");
+        //System.out.println("Kaut kas iekavinas");
+
+        //byte b = 25;
+        //short s = 3000;
+        int i;
+        i = 25;
+        i = 27;
+        int j = 30;
+        i = j;
+        j = 45;
+        boolean some;
+        some = false;
+        char c = 'R';
+
+        //long l = 5000000000L;
+        //float f = 2.57F;
+        //double d = 2.56789;
+
+        //System.out.println(b);
+        //System.out.println(s);
+        //System.out.println(i);
+        //System.out.println(j);
+        //System.out.println(l);
+        //System.out.println(f);
+        //System.out.println(d);
+        System.out.println(some);
+        System.out.println(c);
+    }
+
+}
